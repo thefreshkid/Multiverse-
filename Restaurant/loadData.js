6132ebfa2015049573597e2bf8e2bfa9bc4ffa5f
lawalseymate <60965466+lawalseymate@users.noreply.github.com>
2022-01-13 13:55:39 +0000

    changes

--- Restaurant/loadData.js
+++ Restaurant/loadData.js
@@ -5,13 +5,21 @@ async function load() {
     // wait for the file to be read
     const buffer = await fsp.readFile('./restaurantDB.json');
     const restaurants = (JSON.parse(String(buffer)));
-    const rName = restaurants[0].image;
-    
+    //console.log(restaurants)
+    for (let i = 0; i <restaurants.length; i++){//loop through each restaurant 
+        //console.log (restaurants[i].name)
+        //console.log(restaurants[i].image)
+        for (let j = 0; j <restaurants[i].menus.length; j++){ //loop through each menu of each restaurant 
+        console.log (restaurants[i].menus[j])
+            for (let k = 0; k <restaurants[i])
+        }
+    }
+
 
 
     //console.log(restaurants);
     //return restaurants;
-    console.log(restaurants[0].image);
+    //console.log(restaurants[0].menus[0]);
 
 }
 load();
